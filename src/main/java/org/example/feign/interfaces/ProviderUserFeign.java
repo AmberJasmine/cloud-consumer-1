67f143c018af58c4c94c4ba582c2fcaf986b2cd0
chenlu <1481094533@qq.com>
2024-09-03 20:09:28 +0800

    httpClient的依赖

--- src/main/java/org/example/feign/interfaces/ProviderUserFeign.java
+++ src/main/java/org/example/feign/interfaces/ProviderUserFeign.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * Create by Administrator
  * Data 12:04 2021/12/16 星期四
  */
+//@FeignClient(value = "provider-user", url = "http://123.249.43.78:7900/")
 @FeignClient(value = "provider-user")
 public interface ProviderUserFeign {
 
