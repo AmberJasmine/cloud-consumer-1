449db2cad11d25e2cbc4edfcc936d5ccc652c5bc
chenlu <1481094533@qq.com>
2024-09-09 20:23:24 +0800

    eureka

--- src/test/java/org/example/TestDemoApplicationTest.java
+++ src/test/java/org/example/TestDemoApplicationTest.java
@@ -1,16 +1,25 @@
 package org.example;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 
 @SpringBootTest
 public class TestDemoApplicationTest {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    public void demo() {
-        System.out.println("true = " + true);
-        System.out.println("true = " + true);
+    public void demo() throws SQLException {
+        Connection connection = this.dataSource.getConnection();
+        System.out.println("connection = " + connection);
+        connection.close();
     }
 
 }
